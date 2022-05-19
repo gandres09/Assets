0049d034e5a1c5f260c64ea5b55e65a47fdceed8
Flux <86972937+gandres09@users.noreply.github.com>
2022-05-18 22:22:08 -0300

    Agregar highscore
    Empezando el script para un highscore en la pantalla desde el gameManager

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -11,11 +11,16 @@ public class GameManager : MonoBehaviour
     public int contador;
     public Text textoContador;
 
+    public int contadorHighScore = 0;
+
+    public Text textoHighScore;
+
     // Start is called before the first frame update
     void Start()
     {
         //Inicializo el contador
         textoContador.text = "0";
+        textoHighScore.text = "High Score: " + contadorHighScore;
 
     }
 
@@ -48,6 +53,45 @@ public class GameManager : MonoBehaviour
         cam.transform.position = inicioCamara;
         SceneManager.LoadScene("Menu");
 
+    }
+
+    //se guarda el contador en un archivo para un highscore
+    public void guardarContador()
+    {
+        string path = Application.persistentDataPath + "/contador.txt";
+        System.IO.File.WriteAllText(path, contador.ToString());
+    }
+
+    //se carga el contador del archivo para un highscore
+    public void cargarContador()
+    {
+        string path = Application.persistentDataPath + "/contador.txt";
+        if (System.IO.File.Exists(path))
+        {
+            string contadorString = System.IO.File.ReadAllText(path);
+            contadorHighScore = int.Parse(contadorString);
+        }
+    }
+
+    // usa highscore para ver si el contador es mayor que el highscore
+    public void verificarHighScore()
+    {
+        string path = Application.persistentDataPath + "/contador.txt";
+        if (System.IO.File.Exists(path))
+        {
+            string contadorString = System.IO.File.ReadAllText(path);
+            contadorHighScore = int.Parse(contadorString);
+            if (contador > contadorHighScore)
+            {
+                guardarContador();
+            }
+        }
+    }
+
+    //poner el contadorHighScore en texto
+    public void ponerContadorHighScore()
+    {
+        textoHighScore.text = contadorHighScore.ToString();
     }
 
 }
